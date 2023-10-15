ca51cb621b98fe58352f2dc5d219060ca12d0d01
universaltheory <wtm491@gmail.com>
2023-10-14 21:24:08 -0400

    Initial push

---
+++ DCF.py
@@ -0,0 +1,54 @@
+# Company future valuation prediction program
+# Necessary variables include: 
+# Cash flow, growth rate, rate of return, and timeframe for prediction 
+
+import tkinter as tk
+from tkinter import messagebox
+
+# Function to calculate the future value
+def calculate_future_value():
+    try:
+        cf = float(cf_entry.get())
+        g = float(g_entry.get())
+        r = float(r_entry.get())
+        n = int(n_entry.get())
+        
+        if r == g:
+            raise ValueError("The discount rate (r) cannot be equal to the growth rate (g).")
+        
+        fv = cf * ((1 + g) ** n) / (r - g)
+        result_label.config(text=f"Estimated Future Value: {round(fv, 2)}")
+    except ValueError as e:
+        messagebox.showerror("Invalid Input", str(e))
+
+# Creating the main window
+window = tk.Tk()
+window.title("Company Value Estimator")
+
+# Creating labels and entry fields for the variables
+tk.Label(window, text="Annual Free Cash Flow (CF):").pack()
+cf_entry = tk.Entry(window)
+cf_entry.pack()
+
+tk.Label(window, text="Expected Annual Growth Rate (g) (e.g., 0.05 for 5%):").pack()
+g_entry = tk.Entry(window)
+g_entry.pack()
+
+tk.Label(window, text="Required Rate of Return (r) (e.g., 0.1 for 10%):").pack()
+r_entry = tk.Entry(window)
+r_entry.pack()
+
+tk.Label(window, text="Number of Years to Predict (n):").pack()
+n_entry = tk.Entry(window)
+n_entry.pack()
+
+# Calculate button
+calculate_button = tk.Button(window, text="Calculate", command=calculate_future_value)
+calculate_button.pack()
+
+# Result label
+result_label = tk.Label(window, text="", font=('bold', 14))
+result_label.pack()
+
+# Run the application
+window.mainloop()
